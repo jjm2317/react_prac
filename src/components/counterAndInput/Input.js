62d24967b1adbaabfc7a6af9f1ab6ecabd5d8d3b
jjm2317 <wlaks2317@gmail.com>
2021-04-19 09:48:16 +0900

    feat: focus change when button click

--- src/components/counterAndInput/Input.js
+++ src/components/counterAndInput/Input.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const Input = () => {
+  const defaultInput = useRef();
+  const additionalInput = useRef();
   const [inputValue, setInputValue] = useState({
     defaultValue: "",
     additionalValue: "",
@@ -19,24 +21,40 @@ const Input = () => {
   const onSubmit = (e) => {
     e.preventDefault();
     setResultText(`${inputValue.defaultValue} ${inputValue.additionalValue}`);
+    setInputValue({
+      [defaultInput.current.name]: "",
+      [additionalInput.current.name]: "",
+    });
+    defaultInput.current.focus();
   };
 
-  const onClick = (e) => {
+  const onReset = (e) => {
     e.preventDefault();
-    // setInputText("")
+    setInputValue({
+      [defaultInput.current.name]: "",
+      [additionalInput.current.name]: "",
+    });
+
+    defaultInput.current.focus();
   };
   return (
     <>
       <form onSubmit={onSubmit}>
-        <input name="defaultValue" onChange={onChange} value={defaultValue} />
+        <input
+          name="defaultValue"
+          onChange={onChange}
+          value={defaultValue}
+          ref={defaultInput}
+        />
         <input
           name="additionalValue"
           placeholder="추가값"
           onChange={onChange}
           value={additionalValue}
+          ref={additionalInput}
         />
         <button type="submit">입력</button>
-        <button onClick={onClick}>초기화</button>
+        <button onClick={onReset}>초기화</button>
       </form>
       <p>값 : {resultText}</p>
     </>
