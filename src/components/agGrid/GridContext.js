c3293d507ae3203447a189f83a56a6a377b24050
jjm2317 <wlaks2317@gmail.com>
2021-05-18 02:37:58 +0900

    refactor: (AgGrid) context reducer

--- src/components/agGrid/GridContext.js
+++ src/components/agGrid/GridContext.js
@@ -1,73 +1,29 @@
 import axios from 'axios';
 import React, { createContext, useContext, useReducer } from 'react';
 import * as api from './api';
-import createAsyncDispatcher from './asyncActionUtils';
+import createAsyncDispatcher, { createAsyncHandler, initialAsyncState } from './asyncActionUtils';
 
 const initialState = {
-  linkbooks: {
-    loading: false,
-    data: null,
-    error: null
-  },
-  linkbook: {
-    loading: false,
-    data: null,
-    error: null
-  }
+  linkbooks: initialAsyncState,
+  linkbook: initialAsyncState
 };
 
-const loadingState = {
-  loading: true,
-  data: null,
-  error: null
-};
-
-const success = data => ({
-  loading: false,
-  data,
-  error: null
-});
-
-const error = error => ({
-  loading: false,
-  data: null,
-  error
-});
 // 여러개의 데이터를 하나의 상태로 관리 할 때 액션타입 네이밍의 처리가 달라진다
 // LOADING -> GET_LINKBOOK
+const linkbooksHandler = createAsyncHandler('GET_LINKBOOKS', 'linkbooks');
+const linkbookHandler = createAsyncHandler('GET_LINKBOOK', 'linkbook');
 function linkbooksReducer(state, action) {
   switch (action.type) {
     case 'GET_LINKBOOKS':
-      return {
-        ...state,
-        linkbooks: loadingState
-      };
     case 'GET_LINKBOOKS_SUCCESS':
-      return {
-        ...state,
-        linkbooks: success(action.data)
-      };
     case 'GET_LINKBOOKS_ERROR':
-      return {
-        ...state,
-        linkbooks: error(action.error)
-      };
+      return linkbooksHandler(state, action);
     case 'GET_LINKBOOK':
-      return {
-        ...state,
-        linkbook: loadingState
-      };
     case 'GET_LINKBOOK_SUCCESS':
-      return {
-        ...state,
-        linkbook: success(action.data)
-      };
     case 'GET_LINKBOOK_ERROR':
-      return {
-        ...state,
-        linkbook: error(action.error)
-      };
+      return linkbookHandler(state, action);
     default:
+      throw new Error(`${action.type} `);
   }
 }
 
--- src/components/agGrid/asyncActionUtils.js
+++ src/components/agGrid/asyncActionUtils.js
@@ -13,3 +13,57 @@ export default function createAsyncDispatcher(type, promiseFn) {
   }
   return actionHandler;
 }
+
+export const initialAsyncState = {
+  loading: false,
+  data: null,
+  error: null
+};
+
+const loadingState = {
+  loading: true,
+  data: null,
+  error: null
+};
+
+const success = data => ({
+  loading: false,
+  data,
+  error: null
+});
+
+const error = error => ({
+  loading: false,
+  data: null,
+  error
+});
+
+// 리듀서를 작성할 때 편의성을 높이는 함수
+// type은 액션타입, key는 상태안에 들어있는 특정키를 가리킨다.
+export const createAsyncHandler = (type, key) => {
+  const SUCCESS = `${type}_SUCCESS`;
+  const ERROR = `${type}_ERROR`;
+
+  const handler = (state, action) => {
+    switch (action.type) {
+      case type:
+        return {
+          ...state,
+          [key]: loadingState
+        };
+      case SUCCESS:
+        return {
+          ...state,
+          [key]: success(action.data)
+        };
+      case ERROR:
+        return {
+          ...state,
+          [key]: error(action.error)
+        };
+      default:
+        return state;
+    }
+  };
+  return handler;
+};
