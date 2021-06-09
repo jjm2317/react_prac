ef4c347a968d13c79ca5676c9cee1568142a5ddf
jjm2317 <wlaks2317@gmail.com>
2021-06-10 03:11:55 +0900

    feat: (saga-crud) Publishing

--- crud/index.js
+++ crud/index.js
@@ -1,1 +1,23 @@
-console.log('start');
+const TodoForm = () => (
+  <form>
+    <input type="text" />
+    <button>생성</button>
+  </form>
+);
+
+const TodoList = () => (
+  <ul>
+    <Todo />
+  </ul>
+);
+
+const Todo = () => <li>content</li>;
+
+const App = () => (
+  <>
+    <TodoForm />
+    <TodoList />
+  </>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
